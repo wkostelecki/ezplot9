ce09ef0504dd285deefec3ce9836b9053f6f915c
gambero88 <gabriel.mazzucchi@gmail.com>
2019-02-28 22:28:37 +0000

    added compare_plot to init file

--- ezplot9/__init__.py
+++ ezplot9/__init__.py
@@ -3,3 +3,4 @@ from ezplot9.plot_functions.bar_plot import bar_plot
 from ezplot9.plot_functions.hist_plot import hist_plot
 from ezplot9.plot_functions.box_plot import box_plot
 from ezplot9.plot_functions.scatter_plot import scatter_plot
+from ezplot9.plot_functions.compare_plot import compare_plot
